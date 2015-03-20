487f5bd757851b33ca525069b675a3b4483cc53c
Hakan <deegan@monkii.net>
2015-03-20 08:53:29 +0100

    Test listing following channels.
    Get a list of channels that the user follows.

--- TwitchTV JSON/RestJsonParser.cs
+++ TwitchTV JSON/RestJsonParser.cs
@@ -6,14 +6,60 @@ using Newtonsoft.Json;
 
 namespace TwitchTV_JSON
 {
+    public class TwitchStream
+    {
+        public string Title { get; set; }
+        public string ChannelOwner { get; set; }
+        public int ViewerCount { get; set; }
+        public string image { get; set; }
+    }
+
     public class twitch_json
     {
         public class Links
         {
             public string self { get; set; }
             public string next { get; set; }
+            public string channel { get; set; }
+            public string follows { get; set; }
+            public string commercial { get; set; }
+            public string stream_key { get; set; }
+            public string chat { get; set; }
+            public string features { get; set; }
+            public string subscriptions { get; set; }
+            public string editors { get; set; }
+            public string teams { get; set; }
+            public string videos { get; set; }
         }
 
+        // public class Links2
+        // {
+        //     public string self { get; set; }
+        //     public string follows { get; set; }
+        //     public string commercial { get; set; }
+        //     public string stream_key { get; set; }
+        //     public string chat { get; set; }
+        //     public string features { get; set; }
+        //     public string subscriptions { get; set; }
+        //     public string editors { get; set; }
+        //     public string teams { get; set; }
+        //     public string videos { get; set; }
+        // }
+        // 
+        // public class Links3
+        // {
+        //     public string self { get; set; }
+        //     public string follows { get; set; }
+        //     public string commercial { get; set; }
+        //     public string stream_key { get; set; }
+        //     public string chat { get; set; }
+        //     public string features { get; set; }
+        //     public string subscriptions { get; set; }
+        //     public string editors { get; set; }
+        //     public string teams { get; set; }
+        //     public string videos { get; set; }
+        // }
+        
         // public class Box
         // {
         //     public string template { get; set; }
@@ -21,6 +67,25 @@ namespace TwitchTV_JSON
         //     public string medium { get; set; }
         //     public string large { get; set; }
         // }
+
+        public class Links2
+        {
+            public string self { get; set; }
+        }
+
+        public class Links3
+        {
+            public string self { get; set; }
+            public string follows { get; set; }
+            public string commercial { get; set; }
+            public string stream_key { get; set; }
+            public string chat { get; set; }
+            public string features { get; set; }
+            public string subscriptions { get; set; }
+            public string editors { get; set; }
+            public string videos { get; set; }
+            public string teams { get; set; }
+        }
 
         public class Preview
         {
@@ -36,34 +101,6 @@ namespace TwitchTV_JSON
             public string small { get; set; }
             public string medium { get; set; }
             public string large { get; set; }
-        }
-
-        public class Links2
-        {
-            public string self { get; set; }
-            public string follows { get; set; }
-            public string commercial { get; set; }
-            public string stream_key { get; set; }
-            public string chat { get; set; }
-            public string features { get; set; }
-            public string subscriptions { get; set; }
-            public string editors { get; set; }
-            public string teams { get; set; }
-            public string videos { get; set; }
-        }
-
-        public class Links3
-        {
-            public string self { get; set; }
-            public string follows { get; set; }
-            public string commercial { get; set; }
-            public string stream_key { get; set; }
-            public string chat { get; set; }
-            public string features { get; set; }
-            public string subscriptions { get; set; }
-            public string editors { get; set; }
-            public string teams { get; set; }
-            public string videos { get; set; }
         }
 
         public class Channel
@@ -101,10 +138,22 @@ namespace TwitchTV_JSON
             public double average_fps { get; set; }
             public string created_at { get; set; }
             public Preview preview { get; set; }
-            public Links2 _links { get; set; }
+            public Links3 _links { get; set; }
             public Channel channel { get; set; }
         }
 
+       // public class Stream1
+       // {
+       //     public long _id { get; set; }
+       //     public string game { get; set; }
+       //     public int viewers { get; set; }
+       //     public string created_at { get; set; }
+       //     public int video_height { get; set; }
+       //     public double average_fps { get; set; }
+       //     public Links _links { get; set; }
+       //     public Preview preview { get; set; }
+       //     public Channel channel { get; set; }
+       // }
 
         public class Game
         {
@@ -113,7 +162,7 @@ namespace TwitchTV_JSON
             public int giantbomb_id { get; set; }
             public Preview preview { get; set; }
             public Logo logo { get; set; }
-            public Links2 _links { get; set; }
+            public Links _links { get; set; }
             public int viewers { get; set; }
         }
 
@@ -124,16 +173,24 @@ namespace TwitchTV_JSON
             public Game game { get; set; }
         }
 
+        public class Follow
+        {
+            public string created_at { get; set; }
+            public Links _links { get; set; }
+            public bool notifications { get; set; }
+            public Channel channel { get; set; }
+        }
+
         public class RootObject
         {
             public int _total { get; set; }
             public Links _links { get; set; }
-            public Links2 _links2 { get; set; }
-            public Links3 _links3 { get; set; }
             public List<Top> top { get; set; }
             public List<Channel> channels { get; set; }
             public List<Game> games { get; set; }
-            public List<Stream> streams { get; set; }
+            public Stream stream { get; set; }
+            public List<Follow> follows { get; set; }
+            //public List<StreamSearch> streamsearchs { get; set; }
         }
     }
 }
